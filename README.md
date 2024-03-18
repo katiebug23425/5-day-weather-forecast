af1b44e9b320d6eb8f48e50d2e0f4998bb58b0c3
Katie Bug <katierosealford@gmail.com>
2024-03-18 18:14:14 -0400

    updated to add updated city name when searched

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 5-day-weather-forecast
 a 5 day weather forecast website that can be searched by city name
+
+## Credits
+Xpert Learning Asst
--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ const currentWeatherItemsEl = document.getElementById('currentWeatherItems');
 const weatherForecastEl = document.getElementById('five-day-forecast');
 const searchBtn = document.getElementById('searchBtn');
 const ApiKey = 'e0bac09d544377cea613b7a883eae4f3';
+const updatedCityName = document.getElementById('cityName');
 
 setInterval(() => {
     const time = new Date();
@@ -22,9 +23,9 @@ setInterval(() => {
 searchBtn.addEventListener('click', search);
 
 function search() {
-    let cityName = document.getElementById('citySearch').value;
+    let cityName2 = document.getElementById('citySearch').value;
 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=e0bac09d544377cea613b7a883eae4f3`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName2}&units=imperial&appid=e0bac09d544377cea613b7a883eae4f3`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
@@ -33,18 +34,32 @@ function search() {
 
 
         function showWeatherData(data) {
-            let { temp, humidity, wind_speed } = data.list;
+            let { temp, wind_speed, humidity} = data.list[0].main;
             currentWeatherItemsEl.innerHTML = `
                 <div class="weatherItem">
                     <div>Temp:</div>
                     <div>${temp}&#176</div>
                 </div>
                 <div class="weatherItem">
-                    <div>Wind:</div>
-                    <div>${wind_speed}<span id="mph">MPH</span></div>
-                </div>
+                <div>Wind:</div>
+                <div>${wind_speed}<span id="mph">MPH</span></div>
+            </div>
                 <div class="weatherItem">
                     <div>Humidity:</div>
                     <div>${humidity}%</div>
+                </div>`;
+
+                let name = data.city.name;
+                updatedCityName.innerHTML = `${name}`
+        }
+
+        
+
+        function updateCity(data) {
+            let {wind_speed } = data.list[0].wind;
+            currentWeatherItemsEl.innerHTML = `
+                <div class="weatherItem">
+                    <div>Wind:</div>
+                    <div>${wind_speed}<span id="mph">MPH</span></div>
                 </div>`;
         }
